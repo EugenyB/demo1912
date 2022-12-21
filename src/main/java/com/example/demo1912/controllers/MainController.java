4effbf763edf5a99af8fa5c5757a143d6cb4af74
eugenyb <eberkunsky@gmail.com>
2022-12-21 18:16:37 +0200

    Added from 21.12

--- src/main/java/com/example/demo1912/controllers/MainController.java
+++ src/main/java/com/example/demo1912/controllers/MainController.java
@@ -1,13 +1,17 @@
 package com.example.demo1912.controllers;
 
+import com.example.demo1912.data.Subject;
 import com.example.demo1912.data.Teacher;
 import com.example.demo1912.repositories.SubjectRepository;
 import com.example.demo1912.repositories.TeacherRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,6 +40,25 @@ public class MainController {
             model.addAttribute("teacher", teacher.get());
             model.addAttribute("allsubjects", subjectRepository.findAll());
             return "subjects_by_teacher";
+        }
+    }
+
+    @PostMapping("/add_subject")
+    @Transactional
+    public String addSubject(@RequestParam long teacher_id, @RequestParam long subject) {
+        Optional<Teacher> teacher = teacherRepository.findById(teacher_id);
+        Optional<Subject> subj = subjectRepository.findById(subject);
+        if (teacher.isEmpty() || subj.isEmpty()) {
+            return "error_add_subject";
+        } else {
+            Teacher t = teacher.get();
+            Subject s = subj.get();
+
+            s.getTeachers().add(t);
+
+            teacherRepository.save(t);
+            subjectRepository.save(s);
+            return "redirect:/teachers";
         }
     }
 }
